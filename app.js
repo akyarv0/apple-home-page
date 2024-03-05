2bb3447cc17e82efe971f3111437259244aac5d1
akyarv0 <akyarv0@gmail.com>
2024-03-06 00:10:25 +0300

    fff

--- app.js
+++ app.js
@@ -3,3 +3,29 @@
 document.getElementById("hamburger-menu").addEventListener("click", ({ target }) => {target.classList.toggle("open");});
 //_______________________________//
 
+
+// //? Selectors
+
+const storeListItem = document.querySelector('.list-store');
+const contentDiv = document.querySelector('.content');
+
+const body = document.querySelector('body');
+
+
+function showContent() {
+  contentDiv.style.display = 'flex';
+  body.style.backdropFilter = 'blur(5px)'; 
+}
+
+
+function hideContent() {
+  // contentDiv.style.display = 'none';
+  body.style.backdropFilter = 'none'; // Bulanıklığı kaldırmak için
+}
+
+// Add mouseover event listener
+storeListItem.addEventListener('mouseover', showContent);
+
+// Add mouseout event listener
+storeListItem.addEventListener('mouseout', hideContent);
+
