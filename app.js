bda4e48f3958c92c38cf332eea62ce0eeba58ae5
akyarv0 <akyarv0@gmail.com>
2024-03-05 20:47:07 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+
+//******* Hamburger Menu ********//
+document.getElementById("hamburger-menu").addEventListener("click", ({ target }) => {target.classList.toggle("open");});
+//_______________________________//
+
